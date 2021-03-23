f9dcbf1e0163b330b50b62d7a7884cef74873607
Sergei Kovalenko <sergei.kovalenko@gamigo.com>
2021-03-23 21:15:02 +0100

    API call to get IP

--- src/components/ip-button.tsx
+++ src/components/ip-button.tsx
@@ -8,13 +8,14 @@ export interface ipButtonPropsInterface {
 
 const IpButton = (props: ipButtonPropsInterface) => {
     const [ title, setTitle ] = useState(props.title ?? 'No Title');
-    console.log('IpButton log record');
 
     const button: JSX.Element = (
         <button type="button" className={ipButtonClassName}>{title ?? 'No Title'}</button>
     );
+
     ipChecker.addListener(setTitle);
     ipChecker.start();
+
     return button;
 }
 
--- src/util/ip-checker.ts
+++ src/util/ip-checker.ts
@@ -11,12 +11,21 @@ const addListener = (newListener: StateConverter<string>): void => {
     listeners.push(newListener);
 };
 
-const getIp = (): string => {
-    return 'Who knows?'; // @todo replace with an API call
+const getIp = async (): Promise<string> => {
+    const response = await fetch('https://api.ipify.org', {
+        credentials: 'omit',
+        mode: 'cors',
+        redirect: 'follow',
+    });
+    if (!response.ok) {
+        return 'Error';
+    }
+
+    return await response.text();
 }
 
-const refreshIp = (): void => {
-    const newIp: string = getIp();
+const refreshIp = async (): Promise<void> => {
+    const newIp: string = await getIp();
     if (newIp === current) {
         return;
     }
@@ -45,8 +54,16 @@ const stop = (): void => {
     intervalHandle = 0;
 };
 
-export default {
+export interface IpCheckerInterface {
+    addListener: (newListener: StateConverter<string>) => void,
+    start: () => void,
+    stop: () => void,
+}
+
+const ipChecker: IpCheckerInterface = {
     addListener,
     start,
     stop,
 };
+
+export default ipChecker;
