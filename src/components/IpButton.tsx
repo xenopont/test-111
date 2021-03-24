e1dfcb8edf45b321841870e0865fbd2ea7eaf40b
Sergei Kovalenko <sergei.kovalenko@gamigo.com>
2021-03-24 13:48:09 +0100

    copy button text to clipboard

--- src/components/IpButton.tsx
+++ src/components/IpButton.tsx
@@ -2,15 +2,24 @@ import { ipButtonClassName } from './IpButton/style';
 import IpChecker from '../util/IpChecker';
 import { useState } from 'react';
 
-export interface ipButtonPropsInterface {
+export interface TitleCarrierInterface {
     title?: string;
 }
 
-const IpButton = (props: ipButtonPropsInterface) => {
+const clickHandler = (text: string) => {
+    if (!navigator.clipboard) {
+        console.log('No clipboard API support');
+        return;
+    }
+    navigator.clipboard.writeText(text).then();
+};
+
+const IpButton = (props: TitleCarrierInterface) => {
+    console.log('Button Render');
     const [ title, setTitle ] = useState(props.title ?? 'No Title');
 
     const button: JSX.Element = (
-        <button type="button" className={ipButtonClassName}>{title ?? 'No Title'}</button>
+        <button type="button" onClick={() => { clickHandler(title) }} className={ipButtonClassName}>{title ?? 'No Title'}</button>
     );
 
     IpChecker.addListener(setTitle);
